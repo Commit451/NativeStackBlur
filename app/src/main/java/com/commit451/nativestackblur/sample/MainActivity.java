a029deaf092c24e60e9df5a13c478cfa81675989
John Carlson <jawnnypoo@gmail.com>
2015-09-10 11:50:28 -0500

    Better example image

--- app/src/main/java/com/commit451/nativestackblur/sample/MainActivity.java
+++ app/src/main/java/com/commit451/nativestackblur/sample/MainActivity.java
@@ -2,8 +2,6 @@ package com.commit451.nativestackblur.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -20,34 +18,12 @@ public class MainActivity extends AppCompatActivity {
         mImage = (ImageView) findViewById(R.id.image);
         mBlurredImage = (ImageView) findViewById(R.id.blurredImage);
         Picasso.with(this)
-                .load("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Android_robot.svg/511px-Android_robot.svg.png")
+                .load("http://www.androidcentral.com/sites/androidcentral.com/files/styles/large/public/article_images/2014/10/lollipop-statue-2.jpg?itok=RC1ovcEz")
                 .into(mImage);
 
         Picasso.with(this)
-                .load("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Android_robot.svg/511px-Android_robot.svg.png")
+                .load("http://www.androidcentral.com/sites/androidcentral.com/files/styles/large/public/article_images/2014/10/lollipop-statue-2.jpg?itok=RC1ovcEz")
                 .transform(new BlurTransformation(5))
                 .into(mBlurredImage);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
