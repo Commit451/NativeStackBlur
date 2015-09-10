9ee212b04050bf95d5b41faab6bc57af44ae74d7
John Carlson <jawnnypoo@gmail.com>
2015-09-10 12:20:58 -0500

    Add documentation and bump the version number

--- nativestackblur/src/main/java/com/commit451/nativestackblur/NativeStackBlur.java
+++ nativestackblur/src/main/java/com/commit451/nativestackblur/NativeStackBlur.java
@@ -5,12 +5,16 @@ import android.graphics.Bitmap;
 import com.enrique.stackblur.NativeBlurProcess;
 
 /**
+ * Processes bitmaps using native StackBlur library
  * Created by John on 9/10/15.
  */
 public class NativeStackBlur {
 
     /**
-     * Process the image using a native library
+     * Process the bitmap natively
+     * @param bitmap the bitmap to apply blur to
+     * @param radius the radius of the blur
+     * @return a blurred bitmap
      */
     public static Bitmap process(Bitmap bitmap, int radius) {
         NativeBlurProcess blur = new NativeBlurProcess();
